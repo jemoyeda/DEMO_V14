30b3d602b55930a09589dfd15f7209a9490a635d
Ing. Hermes Colina <79210037+HermesColina2909@users.noreply.github.com>
2022-06-10 15:22:22 -0400

    bienes

--- publicidad_mobiliaria/models/bienes_raices.py
+++ publicidad_mobiliaria/models/bienes_raices.py
@@ -1,11 +1,13 @@
+from typing_extensions import Required
 from odoo import models, fields
 
 class bienes(models.Model):
     _name = 'bienes.raices'
     _description = 'CRM para Bienes Raices'
 
-    titulo = fields.Char(string='Titulo')
-    descripcion  = fields.Text(string='Descripcion')
+    titulo = fields.Char(string='Titulo', required=True)
+    descripcion  = fields.Text(string='Descripcion', required=True)
     codigo_postal  = fields.Char(string='Codigo postal')
-    precio  = fields.Float(string='Precio estimado')
+    precio  = fields.Float(string='Precio estimado', required=True)
+    imagen  = fields.Binary()
     
