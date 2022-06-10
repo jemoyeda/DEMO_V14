33c907b4542aee6d552de5aff8b169a7b0c0cfdd
Ing. Hermes Colina <79210037+HermesColina2909@users.noreply.github.com>
2022-06-10 09:06:42 -0400

    prueba_4

--- publicidad_mobiliaria/models/bienes_raices.py
+++ publicidad_mobiliaria/models/bienes_raices.py
@@ -4,4 +4,4 @@ class bienes_raices(models.Model):
     _name = 'bienes_raices'
     _description = 'CRM bienes raices'
 
-    name = fields.Char(string='Nombre', required=True)
+    titulo = fields.Char(string='')
